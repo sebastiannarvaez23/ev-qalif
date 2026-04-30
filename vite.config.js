2ccc6d125b1a44216bf98599619592323489ce53
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2026-04-29 21:48:42 -0500

    Configuraciones adicionales para deploy gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
+    base: '/ev-qalif/',
     plugins: [react()],
     resolve: {
         alias: {
